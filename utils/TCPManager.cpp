a0f9ce1a1265cc59fe2029615a8dc7d58c82f45d
Richard Min <richardmin97@gmail.com>
2016-05-25 16:58:47 -0700

    TCPManager helper functions added
    next_ack_num generates the next ack number
    nt_seq_num generates the next seq number

--- utils/TCPManager.cpp
+++ utils/TCPManager.cpp
@@ -1,4 +1,5 @@
 #include "TCPManager.h"
+#include "TCPConstants.h"
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,11 +9,12 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include <thread>
 
 TCPManager::TCPManager()
 {
 	srand(time(NULL)); //note that we must do this for our TCP ack/sequence numbers to be random
-	last_syn_num = -1;
+	last_seq_num = -1;
 	last_ack_num = -1;
 	connected_established = false;
 }
@@ -22,18 +24,12 @@ TCPManager::TCPManager()
 }
 
 /**
- * Function: custom_listen(int sockfd, int backlog)
- * Usage: custom_listen(sockfd, 1);
+ * Function: custom_accept(int sockfd, int backlog)
+ * Usage: custom_accept(sockfd, 1);
  * ------------------------------
  * This function blocks, listening for incoming ACKs and responds with a SYN-ACK. 
- * Backlog is ignored in our implementation ()
+ * Backlog is ignored in our implementation (typically would be used for waiting for more connections on queue)
  */
-int TCPManager::custom_listen(int sockfd, int backlog)
-{
-
-	return -1;
-}
-
 int TCPManager::custom_accept(int sockfd, struct sockaddr *addr, socklen_t* addrlen, int flags)
 {
 	return -1;
@@ -44,9 +40,11 @@ int TCPManager::custom_accept(int sockfd, struct sockaddr *addr, socklen_t* addr
  * Usage: custom_connect(sockfd, &addr, sizeof(addr))
  * -----------------------------
  * This function sends a UDP SYN message, and waits for the SYN-ACK to be received. 
+ * It then sends a ACK. 
  */
 int TCPManager::custom_connect(int sockfd, const struct sockaddr * addr, socklen_t addrlen)
 {
+
 	return -1;
 }
 
@@ -60,3 +58,39 @@ int TCPManager::custom_send(int sockfd, void* buf, size_t len, int flags)
 	return -1;
 }
 
+
+/**
+ * Function: next_seq_num()
+ * Usage: next_seq_num(datalen)
+ * ------------------------
+ * This function generates the next syn number: If it's uninitialized, it initializes it. 
+ * Otherwise, it just returns what's stored. Upon receiving a packet one shoudl update last_syn_num
+ * to the appropriate offset (based on byte length).
+ * You pass in how big the packet you are sending is (datawise).
+ * Note that syn/acks are one byte each.
+ */
+
+int TCPManager::next_seq_num(int datalen)
+{
+	//generate the first seq number
+	if(last_seq_num == -1)
+		last_seq_num = rand() % MAX_SEQUENCE_NUMBER;
+	
+	int cached_seq_num = last_seq_num;
+	last_seq_num += datalen;
+	return cached_seq_num;
+}
+
+/**
+ * Function: next_ack_num()
+ * Usage: next_ack_num(len)
+ * -------------------------
+ * This function generates the next ack number. last_ack_num should be set by the connection.
+ */
+int TCPManager::next_ack_num(int datalen)
+{
+	if(!connected_established)
+		return -1;
+	last_ack_num += datalen;
+	return last_ack_num;
+}
--- utils/TCPManager.h
+++ utils/TCPManager.h
@@ -8,7 +8,6 @@
 class TCPManager
 {
 public:
-	int custom_listen(int sockfd, int backlog);
 	int custom_accept(int sockfd, struct sockaddr *addr, socklen_t* addrlen, int flags);
 	int custom_connect(int sockfd, const struct sockaddr * addr, socklen_t addrlen);
 	int custom_recv(int sockfd, void* buf, size_t len, int flags);
@@ -16,11 +15,16 @@ public:
 	TCPManager();
 	~TCPManager();
 private:
-	int last_syn_num;
+	int last_seq_num;
 	int last_ack_num;
 	struct timespec last_received_msg_time;
 
 	bool connection_established;
+
+	int next_seq_num(int datalen);
+	int next_ack_num(int datalen);
+
+	// int update_
 }
 
 
