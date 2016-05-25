6762ca1add6d2059f75abb675473a3cc99dac92f
Richard Min <richardmin97@gmail.com>
2016-05-25 12:01:02 -0700

    Client socketting initialized

--- src/client.cpp
+++ src/client.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -19,28 +21,70 @@
 // http://www.microhowto.info/howto/send_a_udp_datagram_in_c.html
 int main(int argc, char* argv[])
 {
+  srand(time(NULL));  //note that we must do this for our TCP ack/sequence numbers to be random
+
+  int portnum = -1;
+  char* IP;
+  //-------------- Parse Command Line Arguments ----------------//
   if(argc < 3)
   {
     std::cerr << "Usage: client [Server Host or IP] [Port-Number]" << std::endl;
     exit(1);
   }
-	int portnum = -1;
-  char* IP = (char*)malloc((strlen(argv[1]) + 1) * sizeof(char));
-  strcpy(IP, argv[1]);
+	
   std::stringstream convert(argv[2]);
   if(!(convert >> portnum))
   {
     std::cerr << "[Port-Number] must be a valid integer" << std::endl;
     exit(1);
   }
-
-  int r = resolveIP_cstr(IP);
-  if(r == -1)
+  
+  IP = (char*)malloc((strlen(argv[1]) + 1) * sizeof(char));
+  if(IP == NULL)
+  {
+    std::cerr << "Out of memory" << std::endl;
+    exit(1);
+  }
+  strcpy(IP, argv[1]);
+  if(resolveIP_cstr(IP) == -1)
   {
     std::cerr << "IP Failed to resolve! IP passed in was argv[1]" << std::endl;
+    exit(1);
   }
 
-  std::cerr << "web client is not implemented yet" << std::endl;
-  // do your stuff here! or not if you don't want to.
+  //-------------- Set up socket connection ------------//
+  signal(SIGPIPE, SIG_IGN); //Ignore poorly terminated connections from terminating our server
+
+  // create a socket with UDP
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sockfd == -1)
+  {
+    perror("socket");
+    exit(2);
+  }
+
+  // allow others to reuse the address
+  int yes = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    perror("setsockopt");
+    exit(3);
+  }
+
+  // bind address to socket
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(portnum); 
+  addr.sin_addr.s_addr = inet_addr(IP); 
+  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+  //bind the socket
+  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+    perror("bind");
+    exit(4);
+  }
+
   free(IP);
+
+
+  close(sockfd);
 }
